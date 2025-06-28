1157f06e7843537af646acfd6fadab34005a6428
Yuji181181 <s1f102402552@iniad.org>
2025-06-29 03:13:13 +0900

    api init

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,44 +1,7 @@
-import { PrismaClient, Prisma } from "../src/generated/prisma";
+import { PrismaClient, Prisma } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-const userData: Prisma.UserCreateInput[] = [
-  {
-    name: "Alice",
-    email: "alice@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Join the Prisma Discord",
-          content: "https://pris.ly/discord",
-          published: true,
-        },
-        {
-          title: "Prisma on YouTube",
-          content: "https://pris.ly/youtube",
-        },
-      ],
-    },
-  },
-  {
-    name: "Bob",
-    email: "bob@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Follow Prisma on Twitter",
-          content: "https://www.twitter.com/prisma",
-          published: true,
-        },
-      ],
-    },
-  },
-];
+async function main() {}
 
-export async function main() {
-  for (const u of userData) {
-    await prisma.user.create({ data: u });
-  }
-}
-
-main();
+main()
